32e67017e1247ff59c85505c2c8a71077238a8d6
Calomi <cierrios@uc.cl>
2021-11-04 21:09:31 -0300

    adaptacion al personaje camaleon (Mart)

--- nodos.py
+++ nodos.py
@@ -31,31 +31,83 @@ NODOS = { 0: Nodo("Estas en tu habitación, lleno de lujos disfrutando tu delici
                   [Path(13, "Le cuentas al hombre que necesitas ayuda,")]),
           13: Nodo("El hombre resultó ser el alcade de la pequeña localidad, decide ayudarte... Pronto se corre la voz entre los necesitados. Luego de unos días se ha reunido un buen pelotón de gente. "
                    "La gente harta de los malos tratos que resive por parte de la aristocracia decide confiar en tí, ven una pequeña luz de esperanzas de acabar con su sufrimiente.",
-                  [Path(14, "Ir con tus nuevos amigos a derrocar a la aristocracia "), Path(16, "Entrenar más antes de atacar")]),
+                  [Path(14, "Ir con tus nuevos amigos a derrocar a la aristocracia "), Path(26, "Entrenar más antes de atacar")]),
           14: Nodo("Todos los miembros de tu comitiva luchan con ahínco. Sin embargo, la falta de habilidad es notoria.",
                   [Path(15, "seguir luchando contra la gran defensa aristocrática")]),
           15: Nodo("Eres brutalmente superado en fuerza y derrotado",
                   []),
-          16: Nodo("Han pasado unos meses de arduo entrenamiento, haz compartido, sufrido y superados obstaculos junto a tus camaradas "
-                   "finalmente se preparan para marchar al palacio. Luego de una larga marcha y escaramuzas llegas al palacio con una fuerza"
-                   " imparable y suficiente para conquistarlo. Todos cantan por el heroe.",
-                  [Path(17, "Rechazar el canto y lideras la exploración del palacio"), Path(19, "El grupo necesita relajarte, asumir la posición de heroe y unirte a al festejo")]),
+          26: Nodo("Entre los hombres que aparecen se presenta uno particularmente misterioso, Mart, afirma que puede ayudar bastante, "
+                   "pues tiene experiencia militar para entrenar a tus seguidores. Mas, nadie sabe de donde ha salido, parece alguien confiado de sí mismo.\n"
+                   "Te pide dejarle dirigir el entrenamiento. No parece dispuesto a irse...",
+                   [Path(27, "Le permites ayudarte con el entrenamiento de la gente"), Path(30, "No le confias algo tan importante pero le permites quedarse")]),
+          27: Nodo("Han pasado unos meses de arduo entrenamiento, haz compartido, sufrido y superados obstaculos junto a tus camaradas, Mart ha sido"
+                   " de gran ayuda, habeis forjado una buena amistad con él, confias en él pero siempre tiene un aire de misterio.\n"
+                   "Finalmente se preparan para marchar al palacio, pero Mart te dice que tiene asuntos que hacer y debe marcharse antes,"
+                   "te dice que confies en él y se va con una sonrisa que no termina de agradarte...",
+                   [Path(16, "Es hora de partir, inician la marcha.")]),
+          16: Nodo("Luego de una larga marcha de días y escaramuzas llegas al palacio con una fuerza"
+                   " imparable y crees tener fuerza suficiente para conquistarlo.\n"
+                   "Sin embargo en las filas enemigas divisas una figura familiar, es Mart... \n"
+                   "Sus miradas se cruzan, él sonríe desafiante y muy confiado...",
+                   [Path(28, "Esto parece una trampa, Mart conoce nuestras estrategias debemos retirarnos por hoy..."), Path(29,"Motivas a tus tropas a pesar de esta traición, con gran moral atacan el palacio..")]),
+          28: Nodo("Emprendes la retirada, pero tus hombres con esta orden han perdido la moral y retroceden desorganizados, las fuerzas enemigas "
+                   "les dan alcance.",
+                   [Path(15, " Decides utilizar formaciones que no fueron enseñadas por Mart.")]),
+          29: Nodo("Al iniciar la batalla, pierdes de vista a Mart de inmediato, decides conentrarte en la batalla, "
+                   "Luego de horas de batallas comiensas a notar la superioridad que tienes, las tropas enemigas luchan pobremente"
+                   " en contra de tus formaciones, finalmente luego de una ardua batalla logran asegurar el palacio. Todos cantan por el heroe.",
+                  [Path(17, "Rechazar el canto y lideras la exploración del palacio"), Path(19, "El grupo necesita relajarte, asumir la posición de heroe y unirte al festejo")]),
           17: Nodo("Satisfecho con tu desición, caminas por los grandes pasillos del palacio",
                   [Path(18, "Seguir explorando en busca de fugitivos")]),
           18: Nodo("Ingresas a una habitación oscura ubicada en el extremo del palacio. Con la poca luz ingresando a través de las cortinas vez algo brillante en el suelo",
                   [Path(20, "Recoger objeto brillante"), Path(21, "Ajustar ojos a la oscuridad")]),
-          19: Nodo("Te quedas festejando con todos y te a la fiesta que estaba formandose",
-                  [Path(20, "Recoger objeto brillante")]),
+          19: Nodo("Te quedas festejando con todos y te unes a la fiesta que estaba formandose",
+                  [Path(20, "Continuar")]),
           20: Nodo("Sin notarlo, el heroe del otro reino ha caido en la locura y te ataca con una daga envenenada por la espalda",
                   []),
           21: Nodo("Entre las sombras, vez una silueta. Es el heroe del otro reino quien ha caido en la locura y te ataca. Apenas logras defenderte",
                   [Path(22, "luchar"), Path(23, "llamar por ayuda")]),
-          22: Nodo("Sientes que no puedes contra él, pero llega el personaje camaleon quien se sacrifica generando una ventana y permitiendote contratacar ganando la batalla",
-                  [Path(24, "Asistir a personaje camaleon")]),
-          23: Nodo("Mientras llamas por ayuda los ataques del heroe rival te dejan sin aliento. Por suerte, tus gritos fueron escuchados por el personaje camaleon quien se interpone entre tu y el ataque rival recibiendo una herida pero permitiendote contratacar y ganar la batalla",
-                  [Path(24, "Asistir a personaje camaleon")]),
-          24: Nodo("Con sus ultimas palabras, te dice que fue un honor haberte conocido y que ahora si eres un heroe de verdad. Te despides de él entre lagrimas",
+          22: Nodo("Sientes que no puedes contra él, repentinamente aparece Mart quien en un ataque suicida hacia el heroe enemigo generando una ventana y permitiendote contratacar ganando la batalla",
+                  [Path(24, "Asistir a Mart")]),
+          23: Nodo("Mientras llamas por ayuda los ataques del heroe rival te dejan sin aliento. Por suerte, tus gritos fueron escuchados, es Mart quien se interpone entre tu y el ataque rival recibiendo una herida pero permitiendote contratacar y ganar la batalla",
+                  [Path(24, "Asistir a Mart")]),
+          24: Nodo("Con sus ultimas palabras, te dice que fue un honor haberte conocido y que ahora si eres un heroe de verdad al haber confiado en él. Te das cuenta que él enterfirió en las formaciones"
+                   " del enemigo para que pudiesen ganar... Te despides de él entre lagrimas",
                   [Path(25, "Terminar tu misión")]),
           25: Nodo("Unificas Zarene y Cinait. Ganaste",
-                  [])
-        }
+                  []),
+          ###
+          30: Nodo("Han pasado unos meses de arduo entrenamiento, haz compartido, sufrido y superados obstaculos junto a tus camaradas, Mart ha intentado"
+                   " interferir varias veces con sus ideas en el entrenamiento, todo ha marchado bien de todas formas, él es bastante hábil pero siempre tiene un aire de misterio.\n"
+                   "Finalmente se preparan para marchar al palacio, haz perdido de vista a Mart, parce haber desaparecido aunque no le das importancia.",
+                   [Path(31, "Es hora de partir, inician la marcha.")]),
+          31: Nodo("Luego de una larga marcha de días y escaramuzas llegas al palacio con una fuerza"
+                   " imparable y crees tener fuerza suficiente para conquistarlo.\n"
+                   "Sin embargo en las filas enemigas divisas una figura familiar, es Mart... \n"
+                   "Sus miradas se cruzan, él sonríe desafiante y muy confiado...",
+                   [Path(32, "Esto parece una trampa, Mart conoce nuestras estrategias debemos retirarnos por hoy..."), Path(33,"Motivas a tus tropas a pesar de esta traición, con gran moral atacan el palacio..")]),
+          32: Nodo("Emprendes la retirada, pero tus hombres con esta orden han perdido la moral y retroceden desorganizados, las fuerzas enemigas "
+                   "les dan alcance.",
+                   [Path(15, " Decides utilizar formaciones que no haya visto Mart.")]),
+          33: Nodo("Al iniciar la batalla, pierdes de vista a Mart de inmediato, decides conentrarte en la batalla, "
+                   "Luego de horas de batallas comiensas a notar la superioridad que tienes, las tropas enemigas luchan pobremente"
+                   " en contra de tus formaciones, finalmente luego de una ardua batalla logran asegurar el palacio. Todos cantan por el heroe.",
+                  [Path(34, "Rechazar el canto y lideras la exploración del palacio"), Path(19, "El grupo necesita relajarte, asumir la posición de heroe y unirte al festejo")]),
+          34: Nodo("Satisfecho con tu desición, caminas por los grandes pasillos del palacio",
+                  [Path(35, "Seguir explorando en busca de fugitivos")]),
+          35: Nodo("Ingresas a una habitación oscura ubicada en el extremo del palacio. Con la poca luz ingresando a través de las cortinas vez algo brillante en el suelo",
+                  [Path(20, "Recoger objeto brillante"), Path(36, "Ajustar ojos a la oscuridad")]),
+          36: Nodo("Entre las sombras, vez una silueta. Es el heroe del otro reino quien ha caido en la locura y te ataca. Apenas logras defenderte",
+                  [Path(37, "luchar"), Path(38, "llamar por ayuda")]),
+          37: Nodo("Sientes que no puedes contra él, repentinamente aparece Mart quien en un ataque suicida hacia el heroe enemigo generando una ventana y permitiendote contratacar, "
+                   "sin embargo su sincronización contigo no ha sido buena... esto ha sido en vano...",
+                  [Path(15, "Continuar luchando.")]),
+          38: Nodo("Mientras llamas por ayuda los ataques del heroe rival te dejan sin aliento. Repentinamente aparece Mart!, el maldito logró escabullirse... "
+                   "Para tu sorpresa se coloca a tu lado apuntando su espada a tu enemigo.\n"
+                   "Luchan contra el héroe enemigo pero tu y Mart no tienen buena sincronización... si tan solo...\n"
+                   "Debido a esto y al canzancio el enemigo apuñala a Mart en un descuido.",
+                  [Path(39, "Continuar luchando")]),
+          39: Nodo("Debido al cansancio acumulado en tí, no eres capaz de super al enemigo, repentinamente eres atravezado por"
+                   " su espada...",
+                  [Path(15, "Continuar...")]),
+          }
